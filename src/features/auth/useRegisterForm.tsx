57bd44daf4ff10b217105c3f0fcf134d32f40823
verbey <bivanestor@tutanota.com>
2025-12-02 11:05:18 +0000

    Fix deprecated method in useRegisterForm

--- src/features/auth/useRegisterForm.tsx
+++ src/features/auth/useRegisterForm.tsx
@@ -8,7 +8,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const formSchema = z.object({
-    email: z.string().email({ message: "Invalid email address" }).trim(),
+    email: z.email({ message: "Invalid email address" }).trim(),
     username: z.string().min(3, { message: "Username must be at least 3 characters" }),
     password: z.string().min(8, { message: "Password must be at least 8 characters" }),
     confirmPassword: z.string(),
