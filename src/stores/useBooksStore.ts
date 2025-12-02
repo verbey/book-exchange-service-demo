5be16d2bd7c0f486a91ecd6e7cbcb0f3ea2c508c
verbey <bivanestor@tutanota.com>
2025-12-02 12:58:53 +0000

    Use BookState as an interface for consistency

--- src/stores/useBooksStore.ts
+++ src/stores/useBooksStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import type { BookCardProps } from '../features/book-display/types/BookCardProps'
 
-type BooksState = {
+interface BooksState {
     books: BookCardProps[]
     loading: boolean
     error: string | null
